358f2f50682c3d73afee8cb7f1e83d4fe0dafb1a
danbearl <danbearl@gmail>
2012-09-27 08:54:17 -0400

    Created new client form and view.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,16 +4,36 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       format.html #index.html.erb
-      format.json { render :json => @posts }
+      format.json { render :json => @clients }
     end
   end
 
   def new
+    @client = Client.new
+
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render :json => @client }
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  def create
+    @client = Client.new(params[:client])
+
+    respond_to do |format|
+      if @client.save
+        format.html { redirect_to(@client, :notice => 'Client was successfully saved.') }
+        format.json { render :json => @client, :status => :created, :location => :client }
+      else
+        format.html {render :action => "new"}
+        format.json {render :json => @client.errors, :status => :unprocessable_entity}
+      end
+    end
   end
 end
